971b585593ac6b4cd50f68ef71a24a48128c6e53
Edward Guan <edward.guan@mkcorp.com>
2022-02-18 09:50:32 +0800

    first commit

---
+++ src/Ebook.vue
@@ -0,0 +1,218 @@
+<template>
+  <div class="ebook">
+    <title-bar :ifTitleAndMenuShow="ifTitleAndMenuShow"></title-bar>
+    <div class="read-wrapper">
+      <div id="read"></div>
+      <div class="mask">
+        <div class="left" @click="prevPage"></div>
+        <div class="center" @click="toggleTitleAndMenu"></div>
+        <div class="right" @click="nextPage"></div>
+      </div>
+    </div>
+    <menu-bar
+      :ifTitleAndMenuShow="ifTitleAndMenuShow"
+      :fontSizeList="fontSizeList"
+      :defaultFontSize="defaultFontSize"
+      @setFontSize="setFontSize"
+      :themeList="themeList"
+      :defaultTheme="defaultTheme"
+      @setTheme="setTheme"
+      :bookAvailable="bookAvailable"
+      @onProgressChange="onProgressChange"
+      :navigation="navigation"
+      @jumpTo="jumpTo"
+      ref="menuBar"
+    ></menu-bar>
+  </div>
+</template>
+
+<script>
+import TitleBar from '@/components/TitleBar'
+import MenuBar from '@/components/MenuBar'
+import Epub from 'epubjs'
+
+const DOWNLOAD_URL = 'static/Linux_Book.epub' // 载入下载电子书
+
+export default {
+  name: 'Ebook',
+  components: {
+    TitleBar,
+    MenuBar
+  },
+  data () {
+    return {
+      ifTitleAndMenuShow: false, // 是否显示标题栏和菜单
+      fontSizeList: [
+        { fontSize: 12 },
+        { fontSize: 14 },
+        { fontSize: 16 },
+        { fontSize: 18 },
+        { fontSize: 20 },
+        { fontSize: 22 },
+        { fontSize: 24 }
+      ], // 字号列表
+      defaultFontSize: 16, // 默认字号
+      themeList: [
+        {
+          name: 'default',
+          style: {
+            body: {
+              color: '#000 !important', background: '#fff !important'
+            }
+          }
+        },
+        {
+          name: 'eye',
+          style: {
+            body: {
+              color: '#000 !important', background: '#ceeaba !important'
+            }
+          }
+        },
+        {
+          name: 'night',
+          style: {
+            body: {
+              color: '#fff !important', background: '#000 !important'
+            }
+          }
+        },
+        {
+          name: 'gold',
+          style: {
+            body: {
+              color: '#000 !important', background: 'rgb(241,236,226) !important'
+            }
+          }
+        }
+      ], // 主题列表
+      defaultTheme: 0,
+      bookAvailable: false, // 是否可读
+      navigation: null // 目录
+    }
+  },
+  methods: {
+    // 电子书的解析和渲染
+    showEpub () {
+      // 解析电子书,生成Book对象
+      this.book = new Epub(DOWNLOAD_URL)
+      // 通过Book.renderTo生成Rendition对象(对象名不唯一)
+      this.rendition = this.book.renderTo('read', { // 第一个参数是div的id
+        width: window.innerWidth,
+        height: window.innerHeight
+      })
+      // 通过Rendtion.display渲染电子书
+      this.rendition.display()
+      // 获取Theme对象
+      this.themes = this.rendition.themes
+      // 设置默认字体
+      this.setFontSize(this.defaultFontSize)
+      /* thems的俩个方法:
+        主题注册: this.themes.register(name,styles)
+        主题切换: this.themes.select(name)
+      */
+      this.registerTheme()
+      this.setTheme(this.defaultTheme)
+      // 获取Locations对象
+      // 通过epubjs钩子函数来实现电子书定位
+      this.book.ready.then(() => {
+        // 生成目录
+        this.navigation = this.book.navigation
+        return this.book.locations.generate()
+      }).then(result => {
+        this.locations = this.book.locations
+        // this.onProgressChange(50); //实现跳转到百分之50的位置
+        // 标记电子书为解析完毕状态
+        this.bookAvailable = true
+      })
+    },
+    // 翻页功能
+    prevPage () {
+      // rendition.prev方法实现上一页
+      if (this.rendition) {
+        this.rendition.prev()
+      }
+    },
+    nextPage () {
+      // rendition.next方法实现下一页
+      this.rendition.next()
+    },
+    // 标题栏和菜单栏
+    toggleTitleAndMenu () {
+      // 切换
+      this.ifTitleAndMenuShow = !this.ifTitleAndMenuShow
+      if (!this.ifTitleAndMenuShow) {
+        // ref 加在子组件上,获取到的是组件实例,可以使用组件的所有方法,this.$refs.(ref值).组件方法。
+        this.$refs.menuBar.hideSetting()
+      }
+    },
+    // 设置字体大小切换
+    setFontSize (fontSize) {
+      this.defaultFontSize = fontSize
+      if (this.themes) {
+        this.themes.fontSize(fontSize + 'px')
+      }
+    },
+    registerTheme () {
+      this.themeList.forEach(theme => {
+        // 注册全部主题颜色样式
+        this.themes.register(theme.name, theme.style)
+      })
+    },
+    setTheme (index) {
+      // 切换主题
+      this.themes.select(this.themeList[index].name)
+      this.defaultTheme = index
+    },
+    // propress进度条的数值(0-100)
+    onProgressChange (progress) {
+      const percentage = progress / 100
+      const location = percentage > 0 ? this.locations.cfiFromPercentage(percentage) : 0
+      this.rendition.display(location)
+    },
+    hideTitleAndMenu () {
+      // 隐藏标题栏和菜单栏
+      this.ifTitleAndMenuShow = false
+      // 隐藏菜单栏弹出的设置栏
+      this.$refs.menuBar.hideSetting()
+      // 隐藏目录
+      this.$refs.menuBar.hideContent()
+    },
+    // 根据链接跳转到指定位置(目录)
+    jumpTo (href) {
+      this.rendition.display(href)
+      this.hideTitleAndMenu()
+    }
+  },
+  mounted () {
+    // 页面加载后
+    this.showEpub()
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+@import "assets/styles/global";
+.ebook {
+  position: relative;
+  .read-wrapper {
+    .mask {
+      position: absolute;
+      top: 0;
+      left: 0;
+      display: flex;
+      width: 100%;
+      height: 100%;
+      .left {
+        flex: 0 0 px2rem(100);
+      }
+      .center {
+        flex: 1;
+      }
+      .right {
+        flex: 0 0 px2rem(100);
+      }
+    }
+  }
+}
+</style>
---
+++ src/components/TitleBar.vue
@@ -0,0 +1,61 @@
+<template>
+  <transition name="slide-down">
+    <div class="title-wrapper" v-show="ifTitleAndMenuShow">
+      <div class="left">
+        <span class="icon-back iconfont"></span>
+      </div>
+      <div class="right">
+        <div class="icon-wrapper">
+          <span class="icon-cart iconfont"></span>
+        </div>
+        <div class="icon-wrapper">
+          <span class="icon-person iconfont"></span>
+        </div>
+        <div class="icon-wrapper">
+          <span class="icon-more iconfont"></span>
+        </div>
+      </div>
+    </div>
+  </transition>
+</template>
+
+<script>
+export default {
+  props: {
+    ifTitleAndMenuShow: {
+      type: Boolean,
+      default: false
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+@import "../assets/styles/global";
+.title-wrapper {
+  position: absolute;
+  top: 0;
+  left: 0;
+  z-index: 101;
+  width: 100%;
+  display: flex;
+  height: px2rem(48);
+  background: white;
+  box-shadow: 0 px2rem(8) px2rem(8) rgba(0, 0, 0, 0.15);
+  .left {
+    flex: 0 0 px2rem(60);
+    @include center;
+    font-size: px2rem(28);
+  }
+  .right {
+    flex: 1;
+    display: flex;
+    justify-content: flex-end;
+    .icon-wrapper {
+      flex: 0 0 px2rem(40);
+      @include center;
+      font-size: px2rem(28);
+    }
+  }
+}
+</style>
